220ecd62dd1fb203d54c26d2e9422d69a2e87750
Fauziah Syifa <fauziah13siregar@gmail.com>
2023-11-26 17:21:32 +0700

    Update home.php
    Ganti dashboard

--- home.php
+++ home.php
@@ -2,14 +2,13 @@
     <font color="#2133F8"> DASHBOARD</font>
 </b> <br><br>
 <hr color="#2133F8" size="3px" width="100%"><br>
-<font size="+3"><b>Review Singkat Aplikasi</b></font><br><br>
+<font size="+3"><b>Sejarah Singkat SMP Negeri 1 Kedawung</b></font><br><br>
 <font size="+1">
-    <p>Sistem Informasi Akademik SMPN 1 Kedawung merupakan sebuah aplikasi yang berbasis web. Di aplikasi ini,
-        terdapat pembagian user/multi user yaitu admin,guru dan siswa.
-        Selain itu aplikasi ini terdiri dari menu Mata Pelajaran, Jurusan, Standar Kompetensi, Guru, Siswa, Wali Murid,
-        dan Nilai. Diharapkan aplikasi ini dapat memudahkan pekerjaan guru dalam melakukan pendataan serta penilaian
-        siswa.</p>
-    <p>Aplikasi ini menggunakan bahasa pemrograman server side (PHP) dan client side (HTML,CSS,dan JAVA SCRIPT), dan
-        menggunakan program XAMPP, Adobe Dreamweaver CC 2015,dan Google Chrome.</p>
+    <p>Selamat datang di Sistem Informasi Sekolah (SisInfo), aplikasi komprehensif yang dirancang untuk memudahkan pengelolaan data dan informasi sekolah secara efisien dan efektif. 
+        Di sini, Anda dapat mengakses berbagai fitur seperti pengelolaan data siswa, jadwal pelajaran, kehadiran, nilai, dan informasi penting lainnya. 
+        SisInfo juga dilengkapi dengan interface yang user-friendly, memungkinkan staf, guru, dan siswa untuk berinteraksi dan mengakses informasi penting dengan mudah. 
+        Kami berkomitmen untuk terus mengembangkan dan memperbarui aplikasi ini, sehingga dapat terus memenuhi kebutuhan sekolah dan komunitasnya. 
+        Jelajahi berbagai fitur yang kami tawarkan dan rasakan kemudahan dalam mengelola dan mengakses informasi pendidikan dengan SisInfo.
+    </p>
 </font>
 <br>
