c6707729d9f3c15d30110943e5a436f5641cdf77
Fauziah Syifa <fauziah13siregar@gmail.com>
2023-11-26 16:29:54 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 	$host = "localhost";
-	$user = "root";
-	$password = "";
-	$database = "sekolah";
+	$user = "u531566310_kedawung1";
+	$password = "finalIESI1";
+	$database = "u531566310_sekolah";
 	
 	$connect = mysqli_connect($host,$user,$password,$database) or die (mysqli_error($connect));
 	//mysql_select_db($database) or die (mysql_error());
